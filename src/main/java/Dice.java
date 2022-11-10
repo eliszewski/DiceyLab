cb1eb9add742afa1474179baab79c897d39609f4
eliszewski <liszewski31098@gmail.com>
2022-11-09 22:33:56 -0500

    add some stuff

--- src/main/java/Dice.java
+++ src/main/java/Dice.java
@@ -1,4 +1,15 @@
 public class Dice {
+    public int numOfDice;
 
 
+    public Dice(int numOfDice){
+        this.numOfDice = numOfDice;
+    }
+    public Integer tossAndSum(){
+        int result = 0;
+        for (int i = 1; i <= numOfDice; i++) {
+            result += (6 * Math.random() + 1);
+        }
+        return result;
+    }
 }
--- src/main/java/Simulation.java
+++ src/main/java/Simulation.java
@@ -1,5 +1,35 @@
 public class Simulation {
-
-
-
+    private Dice dice;
+    private Bins bins;
+    private int min, max, numberOfSims, numberOfDie;
+    public Simulation (int numberOfDice,int numberOfSimulations) {
+        dice = new Dice(numberOfDice);
+        min = numberOfDice;
+        max = numberOfDice * 6;
+        bins = new Bins(min, max);
+        numberOfDie=numberOfDice;
+        numberOfSims =numberOfSimulations;
+    }
+    public void runSimulations(){
+        for (int i = 0; i< numberOfSims; i++){
+            bins.incrementBin(dice.tossAndSum());
+        }
+    }
+    public void printResults(){
+        String stars="";
+        int result=0;
+        double percent = 0;
+        System.out.println("-----");
+        System.out.println("Simulation of "+ numberOfDie+" dice tossed for "+ numberOfSims +" times.");
+        System.out.println("-----");
+        for (int i=0;i<=(max-min);i++){
+            result=bins.getBin(i+2);
+            percent=((double) result)/ numberOfSims;
+            for (int j=0;j<(Math.round(percent*100));j++){
+                stars+="*";
+            }
+            System.out.println(String.format("%3d  : %10d: %.2f %s",min+i,result, percent, stars));
+            stars="";
+        }
+    }
 }
